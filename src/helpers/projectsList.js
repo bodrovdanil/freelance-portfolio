9eca67c834048d840d7b6f22d01e330d87434d71
Danil Bodrov <danil.b.s@mail.ru>
2024-09-25 17:02:25 +0500

    project

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,62 @@
+import project01 from "./../img/projects/01.jpg"
+import project02 from "./../img/projects/02.jpg"
+import project03 from "./../img/projects/03.jpg"
+import project04 from "./../img/projects/04.jpg"
+import project05 from "./../img/projects/05.jpg"
+import project06 from "./../img/projects/06.jpg"
+import project01Big from "./../img/projects/01-big.jpg"
+import project02Big from "./../img/projects/02-big.jpg"
+import project03Big from "./../img/projects/03-big.jpg"
+import project04Big from "./../img/projects/04-big.jpg"
+import project05Big from "./../img/projects/05-big.jpg"
+import project06Big from "./../img/projects/06-big.jpg"
+
+
+const projects = [
+   { 
+        title: 'Site block',
+        skills: 'HTML, SCSS, BEM',
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+   { 
+        title: 'Landing',
+        skills: 'Angular, js, HTML, CSS',
+        img: project02,
+        imgBig: project02Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+   { 
+        title: 'Roll delivery',
+        skills: 'HTML, CSS, JavaScript',
+        img: project03,
+        imgBig: project03Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+   { 
+        title: 'Dating app',
+        skills: 'React Native',
+        img: project04,
+        imgBig: project04Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+   { 
+        title: 'Gaming streaming portal',
+        skills: 'HTML, SCSS, React',
+        img: project05,
+        imgBig: project05Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+   { 
+        title: 'Gaming community',
+        skills: 'React, PHP, MySql',
+        img: project06,
+        imgBig: project06Big,
+        gitHubLink: 'https://github.com/bodrovdanil',
+    },
+
+
+]
+
+export {projects}
